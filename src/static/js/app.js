f0c8042c8f2f27389763f7aa43758e6b5960f9e2
tuantran <tranquangtuan0308@gmail.com>
2025-10-31 03:36:08 +0700

    add doc

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Hello Tuan Test! Add one above!</p>
+                <p className="text-center">Hello Group 4! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
